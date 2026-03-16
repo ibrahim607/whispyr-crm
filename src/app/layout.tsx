e77013b8f32df6f8a7f9974bb7add761ffc075b9
ibrahim yasin <111618470+ibrahim607@users.noreply.github.com>
2026-03-16 05:38:10 +0200

    project init

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const metadata: Metadata = {
+  title: "CRM Pro",
+  description: "A crm project build to manage customer relationships",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
